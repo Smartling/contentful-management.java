5b5d63bd6f87bbb249339f5f13df499b611ba121
pkryshtop <pkryshtop@smartling@com>
2026-05-21 10:21:01 +0300

    CFUL-1277 fix CMAWebhookFilter license header, field comment, toString

--- src/main/java/com/contentful/java/cma/model/CMAWebhookFilter.java
+++ src/main/java/com/contentful/java/cma/model/CMAWebhookFilter.java
@@ -6,6 +6,12 @@
  * You may obtain a copy of the License at
  *
  * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package com.contentful.java.cma.model;
@@ -18,10 +24,12 @@ import java.util.Map;
 /**
  * Webhook filter constraint. Mirrors the JSON shape Contentful expects in the
  * {@code filters} array of a webhook definition. Currently supports the
- * {@code equals} operator.
+ * {@code equals} operator. The wire format is positional and heterogeneous:
+ * {@code [{"doc": "<path>"}, "<value>"]}.
  */
 public class CMAWebhookFilter {
 
+    // Field name is the JSON property Contentful expects; do not rename without @SerializedName.
     private List<Object> equals;
 
     private CMAWebhookFilter() {
@@ -47,5 +55,9 @@ public class CMAWebhookFilter {
 
     public List<Object> getEquals() {
         return equals == null ? null : new ArrayList<>(equals);
+    }
+
+    @Override public String toString() {
+        return "CMAWebhookFilter { equals = " + equals + " }";
     }
 }
